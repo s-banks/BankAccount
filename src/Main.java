a99b75a85c6a5afd1e209ed4036a76c9e9dd44ad
Shane Banks <shane.banks02@gmail.com>
2022-06-26 15:39:52 -0500

    points system functional

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,15 @@ import accounts.CheckingAccount;
 public class Main {
 	public static void main(String[] args) {
 		CheckingAccount account = new CheckingAccount(100, 0.042, 700);
-		account.status();
-		account.deposit(20);
-		account.status();
+		System.out.println(account.getRewardPoints());
+		if (account.purchase(18.63)) {
+			System.out.println("Purchase successful");
+		} else {
+			System.out.println("Purchase failed");
+		}
+		System.out.println(account.getRewardPoints());
+
+
+
 	}
 }
