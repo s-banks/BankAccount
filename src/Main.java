a2ee8baa3d7af5457331cc6b1c9cad86255e8eeb
Shane Banks <shane.banks02@gmail.com>
2022-06-26 15:29:21 -0500

    testing

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,11 @@
 import accounts.Account;
+import accounts.CheckingAccount;
 
 public class Main {
 	public static void main(String[] args) {
-		Account account = new Account(100, 0.025);
+		CheckingAccount account = new CheckingAccount(100, 0.042);
 		account.status();
-		account.withdraw(45.86);
-		account.status();
-		account.withdraw(62.96);
-		account.status();
-		account.deposit(32.50);
+		account.deposit(20);
 		account.status();
 	}
 }
