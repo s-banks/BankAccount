b10fff394d47a05921807a10cf960d265fcb7279
Shane Banks <shane.banks02@gmail.com>
2022-06-26 16:22:26 -0500

    point system testing good

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,21 @@ import accounts.DiamondCheckingAccount;
 
 public class Main {
 	public static void main(String[] args) {
-		SilverCheckingAccount silver = new SilverCheckingAccount(250, 0.026, 765);
+		SilverCheckingAccount silver = new SilverCheckingAccount(5000, 0.025, 1000);
+		GoldCheckingAccount gold = new GoldCheckingAccount(5000, 0.025, 1000);
+		DiamondCheckingAccount diamond = new DiamondCheckingAccount(5000, 0.025, 1000);
+
+		System.out.println(silver.getRewardPoints());
+		System.out.println(gold.getRewardPoints());
+		System.out.println(diamond.getRewardPoints());
+
+		silver.purchase(50);
+		gold.purchase(50);
+		diamond.purchase(50);
+
+		System.out.println(silver.getRewardPoints());
+		System.out.println(gold.getRewardPoints());
+		System.out.println(diamond.getRewardPoints());
 
 
 
