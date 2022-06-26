7402351494360701a7a87c5cacb0b71ddc32e352
Shane Banks <shane.banks02@gmail.com>
2022-06-26 15:46:22 -0500

    Fee added to savings account withdraws

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,18 @@
 import accounts.Account;
 import accounts.CheckingAccount;
+import accounts.SavingsAccount;
 
 public class Main {
 	public static void main(String[] args) {
-		CheckingAccount account = new CheckingAccount(100, 0.042, 700);
-		System.out.println(account.getRewardPoints());
-		if (account.purchase(18.63)) {
-			System.out.println("Purchase successful");
-		} else {
-			System.out.println("Purchase failed");
-		}
-		System.out.println(account.getRewardPoints());
+		CheckingAccount ca = new CheckingAccount(100, 0.037, 500);
+		SavingsAccount sa = new SavingsAccount(100, 0.045);
+
+		ca.status();
+		sa.status();
+		ca.withdraw(10);
+		sa.withdraw(10);
+		ca.status();
+		sa.status();
 
 
 
--- src/accounts/SavingsAccount.java
+++ src/accounts/SavingsAccount.java
@@ -6,4 +6,16 @@ public class SavingsAccount extends Account{
 	public SavingsAccount(double balance, double interestRate) {
 		super(balance, interestRate);
 	}
+
+	public boolean withdraw(double amount) {
+		double fee = 0.025 * amount;
+		amount =+ fee;
+		if (amount > balance) {
+			return false;
+		}
+		balance -= amount;
+		return true;
+	}
+
+
 }
